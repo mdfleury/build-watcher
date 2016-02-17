c3a48ecd0b66cfc251d441831d2ff73ae92c6248
Matthew Fleury <matthew.fleury@turner.com>
2016-02-17 14:57:11 -0500

    packaging buildchecker

--- buildchecker.go
+++ buildchecker.go
@@ -1,10 +1,9 @@
 // Watch build numbers for Turner sites
 // by Matt Fleury <matthew.fleury@turner.com>
 
-package main
+package buildchecker
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 	"io/ioutil"
@@ -12,19 +11,8 @@ import (
 	"log"
 )
 
-var sites []string = []string {
-	"http://dev.ncaa.com/build-number.txt",
-	"http://qa.ncaa.com/build-number.txt",
-	"http://staging.ncaa.com/build-number.txt",
-	"http://admin.ncaa.com/build-number.txt",
-	"http://gametool.ncaa.com/build-number.txt",
-	"http://carmen-qa.ncaa.com/build.html",
-	"http://carmen.ncaa.com/build.html",
-	"http://carmen-staging.ncaa.com/build.html",
-}
-
-func checkNumber(site string) (string, string) {
-	number := getBuildNumber(site)
+func CheckNumber(site string) (string, string) {
+	number := GetBuildNumber(site)
 	siteUrl, err := url.Parse(site)
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +20,7 @@ func checkNumber(site string) (string, string) {
 	return siteUrl.Host, number
 }
 
-func getBuildNumber(site string)string {
+func GetBuildNumber(site string)string {
 	resp, err := http.Get(site)
 	if err != nil {
 		log.Fatal(err)
@@ -52,11 +40,4 @@ func getBuildNumber(site string)string {
 	tester := regexp.MustCompile(`build.number=([0-9]+)`)
 	number := tester.FindStringSubmatch(text)
 	return number[1]
-}
-
-func main () {
-	for _, site := range sites {
-		host, number := checkNumber(site)
-		fmt.Println(host, ":", number)
-	}
 }
